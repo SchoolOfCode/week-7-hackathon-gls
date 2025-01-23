7f4b11e2450b34567723e9eba727aeb0e6d7f79d
☀️ <sumeya4hmed@gmail.com>
2025-01-23 14:21:31 +0000

    footer and header added to app.js - with very basic css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { Header } from "./components/header/Header";
+import "./App.css";
+import Footer from "./components/footer/Footer";
 
 export default function App() {
-  return <Header />;
+  return (
+    <main>
+      <Header />;
+      <Footer />
+    </main>
+  );
 }
